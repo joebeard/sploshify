280e69121f287e3d3324a7a3cac9dee659273294
Joe Beard <joe_beard@ziffdavis.com>
2016-01-07 15:54:40 +0000

    refactor playlist

--- web_interface/playlist.py
+++ web_interface/playlist.py
@@ -6,28 +6,6 @@ from sploshify_functions import *
 
 cgitb.enable()
 
-min_playlist_size = 10
-
-mysql_username = 'sploshify'
-mysql_password = 'sploshify1'
-mysql_host = 'localhost'
-mysql_db = 'sploshify'
-
-def get_current_playlist(mysql_cursor):
-    mysql_cursor.execute("""SELECT * FROM playlist
-            LEFT JOIN media ON playlist.media_id = media.id
-            WHERE played IS NULL ORDER BY user_selected DESC, playlist.id ASC""")
-    unplayed = list(mysql_cursor.fetchall())
-
-    queue_position = 1
-
-    table = "<table><tr><th>#</th><th>Artist</th><th>Track</th></tr>"
-    for track in unplayed:
-        table += "<tr><td>%s</td><td>%s</td><td>%s</td></tr>" % (queue_position, track[6], track[5])
-        queue_position += 1
-    table += "</table>"
-    return table
-
 def main():
     mysql_connect_object = MySQLdb.connect(host=mysql_host, user=mysql_username, passwd=mysql_password, db=mysql_db)
     mysql_connect_object.autocommit(True)
